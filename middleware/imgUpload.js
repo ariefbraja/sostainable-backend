f8077272dc71ca480179f6fc222f250f9e62e9b0
Rafif1103 <rafifrahmadika@gmail.com>
2024-06-10 23:40:55 +0700

    add api create event and get event

---
+++ middleware/imgUpload.js
@@ -0,0 +1,72 @@
+const {Storage} = require('@google-cloud/storage')
+const fs = require('fs')
+const path = require('path');
+
+const pathKey = path.resolve('./serviceaccountkey.json')
+
+// TODO: Sesuaikan konfigurasi Storage
+const gcs = new Storage({
+    projectId: 'capstone-project-424713',
+    keyFilename: pathKey
+})
+
+// TODO: Tambahkan nama bucket yang digunakan
+const bucketName = 'sostainable'
+const bucket = gcs.bucket(bucketName)
+
+function getPublicUrl(filename) {
+    return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+}
+
+let ImgUpload = {}
+
+ImgUpload.uploadToGcs = (id_event) => {
+    return (req, res, next) => {
+        if (!req.files || req.files.length === 0) return next();
+
+        const folderPath = `images/${id_event}`;
+
+        // Array to store promises for each file upload
+        const uploadPromises = [];
+
+        req.files.forEach((file, index) => {
+            const gcsname = `${folderPath}/${index}`;
+            const fileUpload = bucket.file(gcsname);
+
+            // Create a promise for each file upload
+            const uploadPromise = new Promise((resolve, reject) => {
+                const stream = fileUpload.createWriteStream({
+                    metadata: {
+                        contentType: file.mimetype
+                    }
+                });
+
+                stream.on('error', (err) => {
+                    file.cloudStorageError = err;
+                    reject(err);
+                });
+
+                stream.on('finish', () => {
+                    file.cloudStorageObject = gcsname;
+                    file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+                    resolve();
+                });
+
+                stream.end(file.buffer);
+            });
+
+            uploadPromises.push(uploadPromise);
+        });
+
+        // Wait for all file uploads to complete before proceeding
+        Promise.all(uploadPromises)
+            .then(() => {
+                next();
+            })
+            .catch((err) => {
+                next(err);
+            });
+    };
+};
+
+module.exports = ImgUpload
