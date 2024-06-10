4a5e0c5205584343fc0be4061be02fb1bf7b8d88
ariefbraja <marief.braja@gmail.com>
2024-06-10 21:03:37 +0700

    add API 7, 8, 9, 10

---
+++ routes/profile.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+const jwt = require("jsonwebtoken");
+const jwtGenerator = require("../utils/jwtGenerator");
+
+router.get("", async (req, res) => {
+    try {
+        const authHeader = req.header("Authorization");
+        const token = authHeader && authHeader.split(' ')[1];
+        let verify = jwt.verify(token, process.env.jwtSecret);
+        let username = verify.user.username;
+
+        query = await pool.query("SELECT * FROM PENGGUNA WHERE username = $1", [username]);
+
+        if (query.rows.length == 0) res.json({status: 404, message: "Profil tidak ditemukan"});
+
+        return res.json({status: 201, data: query.rows[0]});
+  
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({status: 500, message: err.message});
+    }
+});
+
+router.post("/update", async (req, res) => {
+    try {
+        const { nama, tanggal_lahir, alamat, no_telepon, no_rekening, nama_bank } = req.body;
+        const authHeader = req.header("Authorization");
+        const token = authHeader && authHeader.split(' ')[1];
+        let verify = jwt.verify(token, process.env.jwtSecret);
+        let username = verify.user.username;
+
+        if (nama.length > 120) return res.json({status: 500, message: "Nama tidak boleh melebihi 120 karakter!"});
+
+        await pool.query("UPDATE PENGGUNA SET username = $1, nama = $2, tanggal_lahir = $3, alamat = $4, no_telepon = $5, no_rekening = $6, nama_bank = $7", 
+          [username, nama , tanggal_lahir, alamat, no_telepon, no_rekening, nama_bank]); 
+
+        const jwtToken = jwtGenerator(username);
+
+        return res.json({status: 200, message: "Berhasil mengubah data profile", token: jwtToken});
+  
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({status: 500, message: err.message});
+    }
+});
+
+module.exports = router;
